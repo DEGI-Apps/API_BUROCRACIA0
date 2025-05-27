c3c8c6e9e3b071b1e1bb9e40f79b2747b4ebeb64
Jose Javier Pinales Alvarado <jose.pinales@map.gob.do>
2025-05-27 13:28:14 -0400

    Creacion de bitacoraLogs no se a creado migracion de controllador

---
+++ API_BASE.Domain/Entities/BitacoraLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_BASE.Domain.Entities
+{
+    public class BitacoraLog
+    {
+
+        public int Id { get; set; }
+
+        public string Usuario { get; set; } = string.Empty;
+
+        public string Accion { get; set; } = string.Empty;
+
+        public string Entidad { get; set; } = string.Empty;
+
+        public string Detalles { get; set; } = string.Empty;
+
+        public DateTime Fecha { get; set; } = DateTime.UtcNow;
+
+    }
+}
---
+++ API_BASE.Infrastructure/Persistence/Configurations/BitacoraLogConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using API_BASE.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_BASE.Infrastructure.Persistence.Configurations
+{
+    public class BitacoraLogConfiguration : IEntityTypeConfiguration<BitacoraLog>
+    {
+        public void Configure(EntityTypeBuilder<BitacoraLog> builder)
+        {
+            builder.ToTable("BitacoraLogs");
+
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Usuario)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(b => b.Fecha)
+                .IsRequired();
+
+            builder.Property(b => b.Accion)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(b => b.Entidad)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(b => b.Detalles)
+                .IsRequired();
+        }
+    }
+}
